cd95c2918b31fc2f1091c718be03ac419f9117c1
HUAQI ZHANG <815457143e@gmail.com>
2021-10-18 19:12:37 -0400

    1

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,17 +1,11 @@
 <template>
-  <Signin />
+
 <router-view/>
 
 </template>
 
 <script>
-import Signin from './views/Signin.vue';
 
-export default {
-    components: {
-        Signin
-    }
-}
 </script>
 
 <style>
