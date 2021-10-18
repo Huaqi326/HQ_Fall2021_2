dad72724d9dc27c80275367e7b5250590e793eab
HUAQI ZHANG <815457143e@gmail.com>
2021-10-18 18:58:53 -0400

    1

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,48 +1,23 @@
 <template>
 <div>
-  <div class="background-img"></div>
-    <a class="sign_in" href="/signin">Sign In</a>
-    <a class="sign_up" href='/signup'>Sign Up</a>
+  <Home />
 </div>
 <router-view/>
 
 </template>
 
 <script>
+import Home from './views/Home.vue';
 
+export default {
+    components: {
+        Home
+    }
+}
 </script>
 
 <style>
-.background-img {
-            background:url(./assets/background.jpeg) no-repeat center center fixed;
-            background-size: cover;
-            filter: blur(5px) brightness(0.7);
-            height: 100vh; 
-        }
-        .sign_in {
-            position: absolute;
-            top: 50%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: #555;
-            color: white;
-            padding: 12px 60px;
-            border: none;
-            cursor: pointer;
-            border-radius: 5px;
-        }
-        .sign_up {
-            position: absolute;
-            top: 60%;
-            left: 50%;
-            transform: translate(-50%, -50%);
-            background-color: #555;
-            color: white;
-            padding: 12px 56px;
-            border: none;
-            cursor: pointer;
-            border-radius: 5px;
-        }
+
 </style>
 
 
