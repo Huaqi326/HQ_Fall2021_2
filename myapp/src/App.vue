b241810bec9d02b0d042c4a7af8d4db7c63d6831
HUAQI ZHANG <815457143e@gmail.com>
2021-10-18 13:36:17 -0400

    1

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,18 +1,10 @@
 <template>
-<div>
-    <Home />
 <router-view/>
-</div>
+
 </template>
 
 <script>
-import Home from './views/Home.vue'
 
-export default {
-    components: {
-        Home
-    }
-}
 </script>
 
 
