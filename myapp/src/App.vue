4cbf94b50929e9e16327a929c1de2329e2d81711
HUAQI ZHANG <815457143e@gmail.com>
2021-11-29 13:28:50 -0500

    project

--- myapp/src/App.vue
+++ myapp/src/App.vue
@@ -1,10 +1,10 @@
 <template>
-<!-- <router-view v-slot="{ Component }"> -->
+<router-view v-slot="{ Component }">
   <!-- <router-view> -->
-  <!-- <keep-alive include="Intro">
+  <keep-alive include="Intro">
     <component :is="Component" />
-  </keep-alive> -->
-<router-view />
+  </keep-alive>
+</router-view>
 </template>
 
 <script>
